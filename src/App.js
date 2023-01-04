ed999311716c0f12130cdedd8e91bb0b374b9ce0
Song Hyunbin <shbin05@naver.com>
2023-01-05 03:57:13 +0900

    http completed and making json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components"
 import axios from 'axios';
 
@@ -6,16 +6,47 @@ const Container = styled.div`
   	background-color: white;
 	display: flex;
 	flex-direction: column;
-	position: relative;
 	width: 100%;
 	align-items: center;
 `;
 
+const Prev = styled.button`
+	background-color: white;
+	border: 1px solid;
+	border-radius: 20px;
+	padding-left: 20px;
+	padding-right: 20px;
+	position: absolute;
+	left: 10%;
+	top: 18px;
+`;
+
+const InfoText = styled.text`
+	align-self: flex-start;
+	margin-left: 10%;
+	font-size: 24px;
+	font-weight: 600;
+`;
+
+const InfoBox = styled.div`
+	background-color: white;
+	width: 80%;
+`;
+
+const OrigImage = styled.img`
+	border: 2px solid rgb(0,0,0);
+	width: 192px;
+	height: 192px;
+	margin-top: 20px;
+	margin-bottom: 20px;
+`;
+
 function App() {
-	const url = "http://127.0.0.1:10250/api/"
+	const url = "http://127.0.0.1:8080/api/"
 	const indexRef = useRef(null)
     let formData;
-	
+
+	const [data, setData] = useState(null)
 
 	const onChange = (e) => {
 		const img = e.target.files[0];
@@ -27,7 +58,7 @@ function App() {
 
     const apiSend = (e) => {
         axios.post(
-            url+"test",
+            url+"main",
             formData
         ).then((res) => {
             console.log(res);
@@ -38,44 +69,58 @@ function App() {
     }
 
 	const testSend = (e) => {
-        fetch(url+"test", {
-			method: 'POST',
+        axios.post(url+"test", {
+			"index": indexRef.current.value
+		},{
 			headers: {
-				'Content-Type': 'application/json',
 				'Access-Control-Allow-Origin': '*',
 				'Access-Control-Allow-Headers': '*'
-			},
-			body: JSON.stringify({
-				"index": indexRef.current.value
-			})
+			}
 		}).then(res => {
-			console.log(res)
+			setData(res.data)
 		}).catch(err => {
 			console.log(err)
 		})
 		console.log('Pressed', indexRef.current.value)
     }
-
-	return (
-		<Container>
-			<h1>Car damage detection</h1>
-			<h4>사진을 선택해주세요</h4>
-			<div>
-				<input type='file' 
-					accept='image/jpg,impge/png,image/jpeg' 
-					name='input_img' 
-					onChange={onChange}>
-				</input>
-                <button onClick={apiSend}>확인</button>
-			</div>
-			<h4>index를 입력해주세요</h4>
-			<div>
-				<input type='text' ref={indexRef}/>
-                <button onClick={testSend}>확인</button>
-			</div>
-
-		</Container>
-	);
+	if (data == null){
+		return (
+			<Container>
+				<h1>Car damage detection</h1>
+				<h4>사진을 선택해주세요</h4>
+				<div>
+					<input type='file' 
+						accept='image/jpg,impge/png,image/jpeg' 
+						name='input_img' 
+						onChange={onChange}>
+					</input>
+					<button onClick={apiSend}>확인</button>
+				</div>
+				<h4>index를 입력해주세요</h4>
+				<div>
+					<input type='text' ref={indexRef}/>
+					<button onClick={testSend}>확인</button>
+				</div>
+	
+			</Container>
+		);
+	}	
+	else{
+		const origImage = data["origImage"]
+		const parts = data["part"].join(", ")
+		return (
+			<Container>
+				<h1>Car damage detection</h1>
+				<Prev onClick={()=>setData(null)}><h2>이전</h2></Prev>
+				<InfoText style={{marginTop: 50}}>Original Image</InfoText>
+				<InfoBox>
+					<OrigImage src={`data:image/jpeg;base64,${origImage}`} />
+				</InfoBox>
+				<InfoText>Damaged Parts: &nbsp; {parts}</InfoText>
+			</Container>
+		)
+	}
+	
 }
 
 export default App;
