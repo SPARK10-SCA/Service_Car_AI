85cfdd15159466d534737c6ecfbff7f5254dc2b5
User <shbin05@naver.com>
2023-01-25 18:23:16 +0000

    make_dataset changed

--- make_dataset.py
+++ make_dataset.py
@@ -37,7 +37,6 @@ def remove_random():
             cnt-=1
         except:
             pass
-        
     medium = [file for file in os.listdir(train_dir+'medium/') if file.endswith('.jpg')]
     cnt=len(medium)
     while cnt>7600:
@@ -47,8 +46,7 @@ def remove_random():
             medium.pop(rand)
             cnt-=1
         except:
-            pass
-        
+            pass       
     low = [file for file in os.listdir(train_dir+'low/') if file.endswith('.jpg')]
     cnt=len(low)
     while cnt>7600:
@@ -59,16 +57,23 @@ def remove_random():
             cnt-=1
         except:
             pass
-        
+       
     print("Done remove_random")
 
 def make_testset():
+    if not os.path.exists(test_dir+'high/'):
+        os.makedirs(test_dir+'high/')
+    if not os.path.exists(test_dir+'medium/'):
+        os.makedirs(test_dir+'medium/')
+    if not os.path.exists(test_dir+'low/'):
+        os.makedirs(test_dir+'low/')
+
     high = [file for file in os.listdir(train_dir+'high/') if file.endswith('.jpg')]
     cnt=len(high)
     while cnt>7000:
         rand = random.randrange(0,cnt)
         try:
-            shutil.move(train_dir+'high/'+high[rand], test_dir+high[rand])
+            shutil.move(train_dir+'high/'+high[rand], test_dir+'high/'+high[rand])
             high.pop(rand)
             cnt-=1
         except:
@@ -78,7 +83,7 @@ def make_testset():
     while cnt>7000:
         rand = random.randrange(0,cnt)
         try:
-            shutil.move(train_dir+'medium/'+medium[rand], test_dir+medium[rand])
+            shutil.move(train_dir+'medium/'+medium[rand], test_dir+'medium/'+medium[rand])
             medium.pop(rand)
             cnt-=1
         except:
@@ -88,7 +93,7 @@ def make_testset():
     while cnt>7000:
         rand = random.randrange(0,cnt)
         try:
-            shutil.move(train_dir+'low/'+low[rand], test_dir+low[rand])
+            shutil.move(train_dir+'low/'+low[rand], test_dir+'low/'+low[rand])
             low.pop(rand)
             cnt-=1
         except:
