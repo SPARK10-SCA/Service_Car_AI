201411ecdf43196a3847daddf8a3cb37129029f6
shbin <shbin05@naver.com>
2023-02-02 17:27:46 +0900

    custom dataset

---
+++ write_custom_csv.py
@@ -0,0 +1,21 @@
+import csv 
+
+cnt=0
+w = open('custom.csv','w', newline='', encoding='utf-8')
+wr = csv.writer(w)
+
+with open("price_dataset_large.csv", "r", encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter="\t")
+    for i, line in enumerate(reader):
+        if i==0:
+            wr.writerow(line)
+        else:
+            price = line[0].split(',')[-1]
+            if price.isdigit() and int(price)<=30000 and cnt<=200000: 
+                wr.writerow(line)
+                cnt+=1
+            if price.isdigit() and int(price)>30000:
+                wr.writerow(line)
+            
+
+print(cnt)
