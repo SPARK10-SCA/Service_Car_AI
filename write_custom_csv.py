c2471aec3dc9a4bc3c86850e6f0bf14003edbd30
shbin <shbin05@naver.com>
2023-02-02 17:50:45 +0900

    custom data

--- write_custom_csv.py
+++ write_custom_csv.py
@@ -8,14 +8,14 @@ with open("price_dataset_large.csv", "r", encoding='utf-8') as f:
     reader = csv.reader(f, delimiter="\t")
     for i, line in enumerate(reader):
         if i==0:
-            wr.writerow(line)
+            wr.writerow(line[0].split(','))
         else:
             price = line[0].split(',')[-1]
             if price.isdigit() and int(price)<=30000 and cnt<=200000: 
-                wr.writerow(line)
+                wr.writerow(line[0].split(','))
                 cnt+=1
             if price.isdigit() and int(price)>30000:
-                wr.writerow(line)
+                wr.writerow(line[0].split(','))
             
 
 print(cnt)
