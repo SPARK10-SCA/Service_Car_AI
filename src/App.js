4f72fbd42f197006ea0a7600d4bba68278b4e0e1
Song Hyunbin <shbin05@naver.com>
2023-02-09 12:38:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,22 +21,37 @@ const Prev = styled.button`
 	top: 18px;
 `;
 
-const InfoText = styled.text`
+const InfoText = styled.h2`
 	align-self: flex-start;
-	margin-left: 10%;
 	font-size: 24px;
 	font-weight: 600;
 `;
 
+const InfoText2 = styled.h2`
+	align-self: flex-start;
+	font-size: 20px;
+	font-weight: 500;
+	text-transform: capitalize;
+`
+
 const InfoBox = styled.div`
 	background-color: white;
-	width: 80%;
+	width: 100%;
+	padding-left: 20%;
 `;
 
 const OrigImage = styled.img`
+	border: 1px solid rgb(0,0,0);
+	width: 256px;
+	height: 256px;
+	margin-top: 20px;
+	margin-bottom: 20px;
+`;
+
+const PartImage = styled.img`
 	border: 2px solid rgb(0,0,0);
-	width: 192px;
-	height: 192px;
+	width: 256px;
+	height: 256px;
 	margin-top: 20px;
 	margin-bottom: 20px;
 `;
@@ -46,7 +61,8 @@ function App() {
 	const indexRef = useRef(null)
     let formData;
 
-	const [data, setData] = useState(null)
+	const [data, setData] = useState(null);
+	const [checkedList, setCheckedList] = useState([]);
 
 	const onChange = (e) => {
 		const img = e.target.files[0];
@@ -65,7 +81,7 @@ function App() {
         }).catch((err) => {
             console.log(err);
         })
-		console.log('Pressed')
+		//console.log('Pressed')
     }
 
 	const testSend = (e) => {
@@ -81,7 +97,7 @@ function App() {
 		}).catch(err => {
 			console.log(err)
 		})
-		console.log('Pressed', indexRef.current.value)
+		//console.log('Pressed', indexRef.current.value)
     }
 	if (data == null){
 		return (
@@ -107,16 +123,83 @@ function App() {
 	}	
 	else{
 		const origImage = data["origImage"]
+		//const origMask = data["origMask"]
 		const parts = data["part"].join(", ")
+		const infoList = data["info"]
+
+		const checkValues = [
+			{id: 1, value: "Breakage"},
+			{id: 2, value: "Crushed"},
+			{id: 3, value: "Scratched"},
+			{id: 4, value: "Separated"}
+		]
+		const handleSelect = (event) => {
+			const value = event.target.value;
+			const isChecked = event.target.checked;
+		 
+			if (isChecked) {
+			  //Add checked item into checkList
+			  setCheckedList([...checkedList, value]);
+			} else {
+			  //Remove unchecked item from checkList
+			  const filteredList = checkedList.filter((item) => item !== value);
+			  setCheckedList(filteredList);
+			}
+		  };
+
 		return (
 			<Container>
 				<h1>Car damage detection</h1>
 				<Prev onClick={()=>setData(null)}><h2>이전</h2></Prev>
-				<InfoText style={{marginTop: 50}}>Original Image</InfoText>
 				<InfoBox>
+					<InfoText style={{marginTop: 50}}>Original Image</InfoText>
 					<OrigImage src={`data:image/jpeg;base64,${origImage}`} />
 				</InfoBox>
-				<InfoText>Damaged Parts: &nbsp; {parts}</InfoText>
+				<InfoBox>
+					<InfoText>Damaged Parts: &nbsp; {parts}</InfoText>
+					<hr/>
+					{
+						infoList.map((index) => (
+							<div>
+								<InfoText>Part: {index.part}</InfoText>
+								<div style={{
+									display: "flex",
+									flexDirection: "row"
+								}}>
+									<PartImage src={`data:image/jpeg;base64,${index.part_img}`}/>
+									<div>
+										{
+											checkValues.map((item) => {
+												return (
+													<div key={item.id}>
+														<input
+															type="checkbox"
+															value={item.value}
+															onChange={handleSelect}
+														/>
+														<label>{item.value}</label>
+													</div>
+												);
+											})
+										}
+									</div>
+								</div>
+								
+								<InfoText>Damage Info</InfoText>
+								<InfoText2>{index.damage_info[0]}</InfoText2>
+								<InfoText2>{index.damage_info[1]}</InfoText2>
+								<InfoText2>{index.damage_info[2]}</InfoText2>
+								<InfoText2>{index.damage_info[3]}</InfoText2>
+								<br/>
+								<InfoText>Repair Method: <InfoText2>{index.repair_method}</InfoText2></InfoText>
+								<InfoText>Repair Cost: <InfoText2>{index.repair_cost} Won</InfoText2></InfoText>
+								<hr/>
+							</div>
+							
+						))
+					}
+				</InfoBox>
+				
 			</Container>
 		)
 	}
