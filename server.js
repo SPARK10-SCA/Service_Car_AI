dbab92fb7d579a0c7418b2fd312090f8abcf6169
Song Hyunbin <shbin05@naver.com>
2023-02-13 20:52:53 +0900

    express added

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const cors = require('cors')
+
+app.set('port', 3001)
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'react-project/build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
+
+app.listen(3001, () => {
+    console.log("Server started on port", app.get('port'));
+})
